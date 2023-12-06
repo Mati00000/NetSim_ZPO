5a67692c52e443cf8a0bfc33e8e924de78f7e5e2
Szymon Klimczak <sklimczak@student.agh.edu.pl>
2023-12-06 19:59:48 +0100

    Pliki nagłówkowe package.hpp i storage_types.hpp

--- netsim_project/include/package.hpp
+++ netsim_project/include/package.hpp
@@ -7,10 +7,12 @@
 class Package {
 public:
     Package();
+
     explicit Package(ElementID ID) : ID_(ID) { assigned_IDs.insert(ID_); }
     Package(Package &&package)  noexcept : ID_(package.ID_) {}
     Package &operator=(Package &&package) noexcept ;
     ElementID get_id() const { return ID_; }
+
     ~Package();
 
 private:
