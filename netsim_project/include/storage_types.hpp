48b7e8aaafe2faf8dbe39ddfd7431ec9fb904564
Szymon Klimczak <sklimczak@student.agh.edu.pl>
2023-12-06 19:40:07 +0100

    Pliki nagłówkowe package.hpp i storage_types.hpp

--- netsim_project/include/storage_types.hpp
+++ netsim_project/include/storage_types.hpp
@@ -21,9 +21,7 @@ public:
     virtual const_iterator end() const = 0;
 
     virtual std::size_t size() const = 0;
-
     virtual bool empty() const = 0;
-
     virtual ~IPackageStockpile() = default;
 };
 
