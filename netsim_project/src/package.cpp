84d101c1d9362dd6eaeec9c6c77066430b058ed5
Maksymilian <makscanfly@gmail.com>
2023-12-08 17:49:18 +0100

    konstruktor i destruktor dla Package

--- netsim_project/src/package.cpp
+++ netsim_project/src/package.cpp
@@ -1,2 +1,19 @@
 #include "package.hpp"
 
+Package::Package() {
+    if(!freed_IDs.empty()){
+        if(!assigned_IDs.empty()){
+            ID_ = *(assigned_IDs.rbegin()) + 1;
+            assigned_IDs.insert(ID_);
+        }
+    } else {
+        ID_ = *(freed_IDs.begin());
+        freed_IDs.erase(ID_);
+        assigned_IDs.insert(ID_);
+    }
+}
+
+Package::~Package(){
+    assigned_IDs.erase(ID_);
+    freed_IDs.insert(ID_);
+}
