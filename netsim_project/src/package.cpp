2f73f6e80aaeccd856a5b0c4af0f662a3cb7542e
Szymon Klimczak <sklimczak@student.agh.edu.pl>
2023-12-08 17:52:42 +0100

    Poprawione

--- netsim_project/src/package.cpp
+++ netsim_project/src/package.cpp
@@ -1,9 +1,16 @@
 #include "package.hpp"
 
+Package::~Package() {
+    assigned_IDs.erase(ID_);
+    freed_IDs.insert(ID_);
+}
 Package::Package() {
     if(!freed_IDs.empty()){
         if(!assigned_IDs.empty()){
             ID_ = *(assigned_IDs.rbegin()) + 1;
+            assigned_IDs.insert(ID_);
+        } else {
+            ID_ = 1;
             assigned_IDs.insert(ID_);
         }
     } else {
@@ -11,9 +18,4 @@ Package::Package() {
         freed_IDs.erase(ID_);
         assigned_IDs.insert(ID_);
     }
-}
-
-Package::~Package(){
-    assigned_IDs.erase(ID_);
-    freed_IDs.insert(ID_);
 }
