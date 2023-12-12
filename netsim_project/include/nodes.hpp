12523e24ee7ccbd550b0377b6f92682b3624cdee
Mati00000 <kaczmarczykm0000@gmail.com>
2023-12-12 23:25:23 +0100

    Zdefiniowanie 2 klas

--- netsim_project/include/nodes.hpp
+++ netsim_project/include/nodes.hpp
@@ -1,5 +1,52 @@
 #ifndef NODES_HPP_
 #define NODES_HPP_
 
+#include "types.hpp"
+#include "package.hpp"
+#include "storage_types.hpp"
+#include "helpers.hpp"
+
+#include <map>
+
+class IPackageReceiver{
+public:
+    using const_iterator = typename IPackageStockpile::const_iterator;
+
+    virtual const_iterator cbegin() const = 0;
+    virtual const_iterator cend() const = 0;
+    virtual const_iterator begin() const = 0;
+    virtual const_iterator end() const = 0;
+
+    virtual void receive_package(Package &&p) = 0;
+    virtual ElementID get_id() const = 0;
+
+    virtual ~IPackageReceiver() = default;
+};
+
+template <typename PreferenceType = double>
+
+class ReceiverPreferences {
+public:
+    using preferences_t = std::map<IPackageReceiver*, PreferenceType>;
+    using const_iterator = typename preferences_t::const_iterator;
+
+    explicit ReceiverPreferences(ProbabilityGenerator pg = probability_generator): pg_(std::move(pg)) {};
+
+    const_iterator cbegin() const {return preferences_t_.cbegin(); }
+    const_iterator cend() const {return preferences_t_.cend(); }
+    const_iterator begin() const {return preferences_t_.begin(); }
+    const_iterator end() const {return preferences_t_.end(); }
+
+    void add_receiver(IPackageReceiver *r);
+    void remove_receiver(IPackageReceiver *r);
+    IPackageReceiver* choose_receiver();
+    const preferences_t& get_preferences() const {return preferences_t_;}
+
+private:
+    ProbabilityGenerator pg_;
+    preferences_t preferences_t_;
+};
+
+
 
 #endif /* NODES_HPP_ */
--- netsim_project/include/types.hpp
+++ netsim_project/include/types.hpp
@@ -4,5 +4,8 @@
 #include <functional>
 
 using ElementID = unsigned long long;
+using Time = unsigned long long;
+using TimeOffset = unsigned long long;
+using ProbabilityGenerator = std::function<double()>;
 
 #endif /* TYPES_HPP_ */
