84beb0c874b2880d50fe59f19dffbb22f6263929
Szymon Klimczak <sklimczak@student.agh.edu.pl>
2023-12-13 19:08:12 +0100

    nodes.hpp

--- netsim_project/include/nodes.hpp
+++ netsim_project/include/nodes.hpp
@@ -8,6 +8,7 @@
 
 #include <map>
 
+
 class IPackageReceiver{
 public:
     using const_iterator = typename IPackageStockpile::const_iterator;
@@ -47,6 +48,79 @@ private:
     preferences_t preferences_t_;
 };
 
+class PackageSender {
+public:
+    ReceiverPreferences receiver_preferences_;
 
+    PackageSender() = default;
+    PackageSender(PackageSender &&pack_sender) = default;
+    void send_package();
+    const std::optional<Package> &get_sending_buffer() const { return bufor_; }
+
+protected:
+    void push_package(Package &&package) { bufor_.emplace(package.get_id()); };
+
+private:
+    std::optional<Package> bufor_ = std::nullopt;
+};
+
+class Storehouse : public IPackageReceiver {
+public:
+    Storehouse(ElementID id, std::unique_ptr<IPackageStockpile> d = std::make_unique<PackageQueue>(PackageQueueType::FIFO)) : id_(id), d_(std::move(d)) {}
+
+    using const_iterator = typename IPackageStockpile::const_iterator;
+
+    const_iterator cbegin() const override { return d_->cbegin(); }
+    const_iterator cend() const override { return d_->cend(); }
+    const_iterator begin() const override { return d_->begin(); }
+    const_iterator end() const override { return d_->end(); }
+
+    void receive_package(Package &&p) override;
+    ElementID get_id() const override { return id_; }
+
+private:
+    ElementID id_;
+    std::unique_ptr<IPackageStockpile> d_;
+};
+
+class Worker : public IPackageReceiver, public PackageSender {
+public:
+    Worker(ElementID id, TimeOffset pd, std::unique_ptr<IPackageQueue> q) : PackageSender(), id_(id), pd_(pd), q_(std::move(q)) {}
+
+    using const_iterator = typename IPackageStockpile::const_iterator;
+
+    const_iterator cbegin() const override { return q_->cbegin(); }
+    const_iterator cend() const override { return q_->cend(); }
+    const_iterator begin() const override { return q_->begin(); }
+    const_iterator end() const override { return q_->end(); }
+
+    void do_work(Time t);
+    TimeOffset get_processing_duration() const { return pd_; }
+    Time get_package_processing_start_time() const { return t_; }
+
+    void receive_package(Package &&p) override;
+    ElementID get_id() const override { return id_; }
+
+private:
+    ElementID id_;
+    TimeOffset pd_;
+    Time t_;
+    std::unique_ptr<IPackageQueue> q_;
+    std::optional<Package> bufor_ = std::nullopt;
+};
+
+class Ramp : public PackageSender {
+public:
+    Ramp(ElementID id, TimeOffset di) : PackageSender(), id_(id), di_(di) {}
+    void deliver_goods(Time t);
+    TimeOffset get_delivery_interval() const { return di_; }
+    ElementID get_id() const { return id_; }
+
+private:
+    ElementID id_;
+    TimeOffset di_;
+    Time t_;
+    std::optional<Package> bufor_ = std::nullopt;
+};
 
 #endif /* NODES_HPP_ */
