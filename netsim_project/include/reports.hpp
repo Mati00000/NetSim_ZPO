e9c126f7756cc519f45098193f9dc994b673da2a
Maksymilian <makscanfly@gmail.com>
2024-01-02 21:55:28 +0100

    simulations done

--- netsim_project/include/reports.hpp
+++ netsim_project/include/reports.hpp
@@ -5,6 +5,25 @@
 #include <ostream>
 
 static void generate_structure_report(const Factory& f, std::ostream& os);
+
 static void generate_simulation_turn_report(const Factory& f, std::ostream& os, Time t);
+
+class IntervalReportNotifier {
+public:
+    explicit IntervalReportNotifier(TimeOffset to) : to_(to) {}
+    bool  should_generate_report(Time t) { return (t - 1) % to_ == 0; }
+
+private:
+    TimeOffset to_;
+};
+
+class SpecificTurnsReportNotifier {
+public:
+    explicit SpecificTurnsReportNotifier(std::set<Time> turns) : turns_(std::move(turns)) {}
+    bool should_generate_report(Time t) { return turns_.find(t) != turns_.end(); }
+
+private:
+    std::set<Time> turns_;
+};
 
 #endif /* REPORTS_HPP_ */
--- netsim_project/include/simulation.hpp
+++ netsim_project/include/simulation.hpp
@@ -1,6 +1,8 @@
 #ifndef SIMULATION_HPP_
 #define SIMULATION_HPP_
 
+#include "factory.hpp"
 
+void simulate(Factory& f,TimeOffset d,const std::function<void (Factory&, Time)>& rf);
 
 #endif /* SIMULATION_HPP_ */
